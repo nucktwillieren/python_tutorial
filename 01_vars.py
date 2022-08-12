5ba7e480b5d35340eecf1a005e547c8e3e92a780
Nathan <nucktwillieren@gmail.com>
2022-08-13 05:24:02 +0800

    vars

---
+++ 01_vars.py
@@ -0,0 +1,56 @@
+def main():
+    print("hello, world")  # print a string
+
+    print(1..is_integer())  # interger object
+
+    print("hello, world".replace("e", "b"))  # string object
+
+    print(1 + 1)  # 2
+
+    print(1 * 2)  # 2
+
+    print(2 ** 3)  # 8
+
+    print(2 / 3)  # 0.666666666666 round-off error vs truncation error
+    # ieee 754
+    # 大數相除以整數解
+
+    print(3 / 2)  # 1.5
+
+    print(3 // 2)  # 1
+
+    print(float(1).is_integer())  # true
+
+    print(float(1.).is_integer())  # true
+
+    print(float(1.0).is_integer())  # true
+
+    print(float(1.1).is_integer())  # false
+
+    print(",".join(["1", "2", "3", "4"]))  # "1,2,3,4"
+
+    print("c".replace("c", "d"))  # d
+
+    print("efg".startswith("f"))  # false
+
+    print("hi".endswith("i"))  # true
+
+    print(" hi ".rstrip())  # hi
+
+    print(" hi ".lstrip())  # hi
+
+    print(" hi ".strip())  # hi
+
+    print("1,2,3,4".split(","))  # ['1', '2', '3', '4']
+
+    print("hi".upper())  # HI
+
+    print("HI".lower())  # hi
+
+    print("hi".capitalize())  # Hi
+
+    print("a" + "b")  # ab
+
+
+if __name__ == "__main__":
+    main()
